5fd9ede1501756d02240bd30a25f0ed9fdb8d5a4
327485253 <flash327485253@gmail.com>
2017-01-18 11:46:43 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,3 +1,11 @@
 # new_file.py
+from bs4 import BeautifulSoup
 import re
 re.search("^(/wiki)")
+file = open("aac.txt","w")
+file.write("fuck it ,hahahah ")
+lis = lsit()
+for c in lsit:
+  print(c[1])
+print("hahahhaha" )
+
